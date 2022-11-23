0c995d05a29771e223bec344dc3f696b06231662
Sulaiman <sulaimanchapti786@gmail.com>
2022-11-23 08:56:57 +0530

    Optimized code

--- Contollers/chat.js
+++ Contollers/chat.js
@@ -1,5 +1,5 @@
 const jwt = require('jsonwebtoken')
-
+const User=require('../models/user')
 const Chat=require('../models/Chat')
 
 exports.postMessage = async(req,res,next)=>{
@@ -24,10 +24,17 @@ try {
 }
 
 exports.getMessage = async(req,res,next)=>{
+
+    let msgId = req.query.msg ;
     try {
         const data =await Chat.findAll({where:{userId:req.user.id}})
+        console.log(data.length)
+        let index = data.findIndex(chat => chat.id == msgId)
+        console.log("------",index);
+        let messagestosend = data.slice(index+1)
+        console.log("BRO-------------ll",messagestosend)
         const userName=req.user.name
-        res.status(200).json({data, userName})
+        res.status(200).json({messagestosend, userName})
         
     } catch (err) {
         res.status(500).json({message:'unable to get chats'})
--- Views/Chat/chat.js
+++ Views/Chat/chat.js
@@ -5,60 +5,108 @@ const logoutt=document.getElementById('logout')
 
 
 const chatContainer = document.querySelector('.chat-container-div'); 
-const name = localStorage.getItem('name')
+const User = localStorage.getItem('name')
 
+let lastId;
+let chatArray = []
 
 window.addEventListener('DOMContentLoaded', loadScreen)
 
 async function loadScreen(e){
     e.preventDefault();
-    setInterval(async () => {
+
+    document.getElementById('username').innerHTML = User
+
+    const messages = JSON.parse(localStorage.getItem('msg'));
+        // console.log(messages[messages.length-1].id);
+        if(messages == undefined || messages.length == 0) {
+            lastId = 0;
+        }
+        else {
+            lastId = messages[messages.length-1].id;
+        }
+
+    // setInterval(async () => {
         try {
-            const response =  await axios.get(`http://localhost:3000/getMessage`  , {headers:{"Authorization" : token}})
-            showChatsOnScreen(response.data , response.data.userName);
+            // const response =  await axios.get(`http://localhost:3000/getMessage`  , {headers:{"Authorization" : token}})
+            // showChatsOnScreen(response.data , response.data.userName);
+            const response =  await axios.get(`http://localhost:3000/getMessage?msg=${lastId}`  , {headers:{"Authorization" : token}})
+            console.log(response)
+            var newArr = response.data.messagestosend
+            saveToLocal(newArr , response.data.userName);
 
         } catch (err) {
             console.log(err);
         }
-    },1000)
+    // },1000)
 }
 
-function showChatsOnScreen(data , name){
+
+
+function saveToLocal(arr,name){
+
+    let oldMessages = JSON.parse(localStorage.getItem('msg'));
+    console.log(oldMessages)
+
+    if(oldMessages == undefined || oldMessages.length == 0){
+        chatArray = chatArray.concat(arr)
+    }else{
+        chatArray =[]
+        chatArray = chatArray.concat(oldMessages,arr);
+    }
+    localStorage.setItem('msg' , JSON.stringify(chatArray))
+    console.log((JSON.parse(localStorage.getItem('msg'))).length)
+    showChatsOnScreen(name)
+}
+
+function showChatsOnScreen( name){
 
     chatContainer.innerHTML = ""
 
     localStorage.setItem('name' , name)
-    data.data.forEach(chat =>{
-        showChats(chat , name)
-    } )
-}
+    // data.data.forEach(chat =>{
+    //     showChats(chat , name)
+    // } )
 
-function showChats(chat , name){
 
-    let child = `<div class="msg-div">
+    chatArray.forEach(chat =>{
+
+        let child = `<div class="msg-div">
     <div class="resize-sent">
-      <div class="sent">
+      <div class="sent" id=${chat.id}>
         <p class="sent-name">${name.split(' ')[0]}</p>
         <p class="sent-msg">${chat.message}</p>
-        <p class="sent-time">${chat.createdAt.split('T')[1].slice(0,5)}</p>
-      </div>
-    </div>
-  </div>`
+        <p class="sent-time">${chat.createdAt.split('T')[1].slice(0,5)}</p>`
 
   chatContainer.innerHTML += child
 
+    })
+    document.getElementById(`${lastId}`).scrollIntoView()
 }
 
 
+// function showChats(chat , name){
 
+//     let child = `<div class="msg-div">
+//     <div class="resize-sent">
+//       <div class="sent">
+//         <p class="sent-name">${name.split(' ')[0]}</p>
+//         <p class="sent-msg">${chat.message}</p>
+//         <p class="sent-time">${chat.createdAt.split('T')[1].slice(0,5)}</p>
+//       </div>
+//     </div>
+//   </div>`
 
+//   chatContainer.innerHTML += child
 
-
+// }
 
 logoutt.addEventListener('click', logout)
 function logout(){
 window.location.href='../login.html'
 localStorage.removeItem('Usertoken')
+localStorage.removeItem('msg')
+localStorage.removeItem('name')
 
 }
 
@@ -75,6 +123,8 @@ document.getElementById('chat-form').onsubmit = async function(e){
         const response = await await axios.post(`http://localhost:3000/PostMessage` , message  , {headers:{"Authorization" : token}})
         console.log(response);
         messageInput.value = ""
+        let name = localStorage.getItem('name')
+        saveToLocal(response.data.data , name);
         // showOnScreen( message )
     } catch (err) {
         console.log(err);
